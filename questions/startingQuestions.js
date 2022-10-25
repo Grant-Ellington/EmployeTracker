4e685f4576521e5fc952f8c446ede1821521dd2e
Grant-Ellington <gellingtonem6@gmail.com>
2022-10-25 11:36:53 -0500

    added starting questions

---
+++ questions/startingQuestions.js
@@ -0,0 +1,16 @@
+const startingQuestion = [
+    {
+        type: 'list',
+        name: 'options',
+        message: 'Choose from the following options.',
+        Choices:[
+            'view all departments',
+            'view all roles',
+            'view all employees',
+            'add a department',
+            'add a role',
+            'add an employee',
+            'update'
+        ] 
+    }
+]
